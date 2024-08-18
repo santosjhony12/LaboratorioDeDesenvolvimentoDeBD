adcce9fc4de4ab4fffeb1b9d94471a8bf8594945
santosjhony12 <santosdesouzajhony@gmail.com>
2024-08-18 14:39:56 -0300

    :tada: new Spring project

---
+++ projeto-spring20242/src/main/java/br/gov/sp/fatec/projeto_spring20242/ProjetoSpring20242Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projeto_spring20242;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring20242Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring20242Application.class, args);
+	}
+
+}
---
+++ projeto-spring20242/src/test/java/br/gov/sp/fatec/projeto_spring20242/ProjetoSpring20242ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projeto_spring20242;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpring20242ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
